d121a4aded785d601dae898ebfc10c8888c945dc
ASUS <se.zanganeh@yahoo.com>
2020-10-14 00:50:01 +0330

    21-create bottomNavigationBar way(2) finished

--- lib/HomePage21.dart
+++ lib/HomePage21.dart
@@ -1,90 +1,90 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_exercise/rocket21Pages/home_Page.dart';
-import 'rocket21Pages/accountBox_Page.dart';
-import 'rocket21Pages/addBox_Page.dart';
-import 'rocket21Pages/favorite_Page.dart';
-import 'rocket21Pages/search_Page.dart';
-
-class MyHomePages20 extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomePageState20();
-  }
-}
-
-class MyHomePageState20 extends State<MyHomePages20> {
-  String currentPageName = 'home';
-
-  final Map<String, Widget> children = <String, Widget>{
-    'home': new home_Page(),
-    'search': new search_Page(),
-    'addBox': new addBox_Page(),
-    'favorite': new favorite_Page(),
-    'accountBox': new accountBox_Page(),
-  };
-
-  final appBar = new AppBar(
-    backgroundColor: new Color(0xfff8f8f8),
-    elevation: 1.0,
-    centerTitle: true,
-    leading: new Icon(Icons.camera_alt),
-    title: SizedBox(
-      height: 40.0,
-      child: new Image.asset("assets/images/insta_logo.png"),
-    ),
-    actions: <Widget>[
-      Padding(
-          padding: EdgeInsets.only(right: 12.0), child: new Icon(Icons.send)),
-    ],
-  );
-
-  changePage(String namePage) {
-    setState(() {
-      currentPageName = namePage;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: appBar,
-      body: this.children[currentPageName],
-      bottomNavigationBar: new Container(
-        height: 50,
-        color: Colors.white,
-        child: new BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              new IconButton(
-                  icon: new Icon(Icons.home, color: Colors.black),
-                  onPressed: () {
-                    changePage('home');
-                  }),
-              new IconButton(
-                  icon: new Icon(Icons.search, color: Colors.black),
-                  onPressed: () {
-                    changePage('search');
-                  }),
-              new IconButton(
-                  icon: new Icon(Icons.add_box, color: Colors.black),
-                  onPressed: () {
-                    changePage('addBox');
-                  }),
-              new IconButton(
-                  icon: new Icon(Icons.favorite, color: Colors.black),
-                  onPressed: () {
-                    changePage('favorite');
-                  }),
-              new IconButton(
-                  icon: new Icon(Icons.account_box, color: Colors.black),
-                  onPressed: () {
-                    changePage('accountBox');
-                  })
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_exercise/rocket21Pages/home_Page.dart';
+// import 'rocket21Pages/accountBox_Page.dart';
+// import 'rocket21Pages/addBox_Page.dart';
+// import 'rocket21Pages/favorite_Page.dart';
+// import 'rocket21Pages/search_Page.dart';
+//
+// class MyHomePages20 extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return MyHomePageState20();
+//   }
+// }
+//
+// class MyHomePageState20 extends State<MyHomePages20> {
+//   String currentPageName = 'home';
+//
+//   final Map<String, Widget> children = <String, Widget>{
+//     'home': new home_Page(),
+//     'search': new search_Page(),
+//     'addBox': new addBox_Page(),
+//     'favorite': new favorite_Page(),
+//     'accountBox': new accountBox_Page(),
+//   };
+//
+//   final appBar = new AppBar(
+//     backgroundColor: new Color(0xfff8f8f8),
+//     elevation: 1.0,
+//     centerTitle: true,
+//     leading: new Icon(Icons.camera_alt),
+//     title: SizedBox(
+//       height: 40.0,
+//       child: new Image.asset("assets/images/insta_logo.png"),
+//     ),
+//     actions: <Widget>[
+//       Padding(
+//           padding: EdgeInsets.only(right: 12.0), child: new Icon(Icons.send)),
+//     ],
+//   );
+//
+//   changePage(String namePage) {
+//     setState(() {
+//       currentPageName = namePage;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: appBar,
+//       body: this.children[currentPageName],
+//       bottomNavigationBar: new Container(
+//         height: 50,
+//         color: Colors.white,
+//         child: new BottomAppBar(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: <Widget>[
+//               new IconButton(
+//                   icon: new Icon(Icons.home, color: Colors.black),
+//                   onPressed: () {
+//                     changePage('home');
+//                   }),
+//               new IconButton(
+//                   icon: new Icon(Icons.search, color: Colors.black),
+//                   onPressed: () {
+//                     changePage('search');
+//                   }),
+//               new IconButton(
+//                   icon: new Icon(Icons.add_box, color: Colors.black),
+//                   onPressed: () {
+//                     changePage('addBox');
+//                   }),
+//               new IconButton(
+//                   icon: new Icon(Icons.favorite, color: Colors.black),
+//                   onPressed: () {
+//                     changePage('favorite');
+//                   }),
+//               new IconButton(
+//                   icon: new Icon(Icons.account_box, color: Colors.black),
+//                   onPressed: () {
+//                     changePage('accountBox');
+//                   })
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/rocket20_instaApp.dart
+++ lib/rocket20_instaApp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_exercise/HomePage21.dart';
+import 'package:flutter_exercise/HomePage21Way2.dart';
 
 class MyApp20 extends StatelessWidget {
   @override
@@ -8,11 +9,10 @@ class MyApp20 extends StatelessWidget {
       title: 'اینستاگرام',
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
+          primaryColor: Colors.black,
           primaryIconTheme: IconThemeData(color: Colors.black),
           primaryTextTheme: TextTheme(title: TextStyle(color: Colors.black))),
       home: MyHomePages20(),
     );
   }
 }
-
-
